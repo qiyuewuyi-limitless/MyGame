69aa9b3936284bfc2a1961490ee23ad82da07333
qiyuewuyi-o <a1053264650@163.com>
2024-10-03 18:57:29 +0800

    方向变更
    方向变更

--- Assets/Mythril2D/Core/Runtime/Scripts/Miscellaneous/FollowMoveDirection.cs
+++ Assets/Mythril2D/Core/Runtime/Scripts/Miscellaneous/FollowMoveDirection.cs
@@ -23,12 +23,36 @@ namespace Gyvr.Mythril2D
         {
             if (m_target != null)
             {
+                m_target.directionChangedEventOfMe.AddListener(OnTargetDirectionChanged);
                 m_target.directionChangedEvent.AddListener(OnTargetDirectionChanged);
             }
 
             m_initialPosition = transform.localPosition;
         }
+        public void OnTargetDirectionChanged(Vector2 direction)
+        {
+            EDirection myEDirection = direction.x >= 0.0f ? EDirection.Right : EDirection.Left;
+            float modifier = myEDirection == EDirection.Right ? 1.0f : -1.0f;
+            if (m_strategy == EFollowStrategy.FlipSprites)
+            {
+                transform.localPosition = new Vector3(m_initialPosition.x * modifier, m_initialPosition.y, m_initialPosition.z);
 
+                if (m_toFlip != null)
+                {
+                    foreach (SpriteRenderer spriteRenderer in m_toFlip)
+                    {
+                        spriteRenderer.flipX = myEDirection == EDirection.Left;
+                    }
+                }
+            }
+            else
+            {
+                float angle = math.atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                Debug.Log("angle is " + angle);
+                transform.localRotation = Quaternion.Euler(0.0f,0.0f,myEDirection == EDirection.Left ? angle - 180.0f : angle);
+                transform.localScale = new Vector3(math.abs(transform.localScale.x) * modifier, transform.localScale.y, transform.localScale.z);
+            }
+        }
         public void OnTargetDirectionChanged(EDirection direction)
         {
             float modifier = direction == EDirection.Right ? 1.0f : -1.0f;

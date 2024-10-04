7ce7b6c0591209fe0ab6763ab5f9c7f0a6afc208
Atl212 <56058541+Atl212@users.noreply.github.com>
2024-10-05 00:24:59 +0900

    2024-10-03-00:24-Door Interactable

---
+++ Assets/Mythril2D/Core/Runtime/Scripts/Utils/EObjectAction.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gyvr.Mythril2D
+{
+    [Serializable]
+    public enum EObjectAction
+    {
+        Switch,
+        Activate,
+        Deactivate
+    }
+}
+
